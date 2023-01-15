8b85d3b41448d1fbc477cf08b90bc76b24280117
Taluna Mendes <88059577+talunamendes@users.noreply.github.com>
2023-01-15 01:11:59 -0300

    Update cases.sql
    Change bq dataset id

--- models/cases.sql
+++ models/cases.sql
@@ -1,1 +1,1 @@
-select * from `modern-data-stack-demo.demo_dataset.covid_data` where new_recovered > new_confirmed
+select * from `compact-works-374801.airbyte_demo_dataset.covid_data` where new_recovered > new_confirmed
