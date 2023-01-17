ab310eb58a25f0b449ff4e37dee0a920f44c9f04
Taluna Mendes <taluna.mendes_thoughtworks@prestador.globo>
2023-01-17 00:43:49 -0300

    Refactor sources config and model sql

--- models/airbnb_neighbourhood.sql
+++ models/airbnb_neighbourhood.sql
@@ -2,14 +2,12 @@
 
 {{ config(
     materialized="table",
-    database="compact-works-374801",
     schema="fiap_fase04_curated"
 ) }}
 
 with neighbourhood_normalized as (
     select
         REGEXP_REPLACE(NORMALIZE(upper(neighbourhood), NFD), r'\pM', '') as neighbourhood_name
-    --from `compact-works-374801.fiap_fase04_raw.airbnb_neighbourhood`
     from {{ source('bq_raw', 'airbnb_neighbourhood') }}
     order by neighbourhood
 
